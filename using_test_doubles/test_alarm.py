e69a8cb2c6caaabdaceadb4d84bf4298bb6bf665
Kubilay İşen <isen.kubilay@gmail.com>
2021-02-18 22:53:09 +0300

    added monkeypatching testing

--- using_test_doubles/test_alarm.py
+++ using_test_doubles/test_alarm.py
@@ -1,5 +1,5 @@
 from sensor import Sensor
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from alarm import Alarm
 
 def test_alarm_is_off_by_default():
@@ -22,3 +22,15 @@ def test_normal_pressure_alarm_stays_off():
     alarm = Alarm(stub_sensor)
     alarm.check()
     assert not alarm.is_alarm_on
+
+##Monkeypatching Testing
+
+@patch("alarm.Sensor")
+def test_alarm_with_too_low_pressure_value(test_sensor_class):
+    test_sensor_instance = Mock()
+    test_sensor_instance.sample_pressure.return_value = 16 
+    test_sensor_class.return_value = test_sensor_instance
+    alarm = Alarm()
+
+    alarm.check()
+    assert alarm.is_alarm_on
